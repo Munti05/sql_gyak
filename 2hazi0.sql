bbb4302fc106c7c7f27a1c4ad64c5d0b1f810a3e
Munti05 <152182905+Munti05@users.noreply.github.com>
2024-04-18 16:07:09 +0200

    Update 2hazi0.sql
    usernev átirva

--- 2hazi0.sql
+++ 2hazi0.sql
@@ -11,10 +11,10 @@ SZUL_DAT date MASKED WITH (FUNCTION = 'default()')
 INSERT INTO qas
 (USERNEV, NEV, EMAIL, SZAML_CIM, SZUL_DAT)
 VALUES
-('Dinesh','Asanka','Dineshasanka@gmail.com','Szegöd','2020-01-01'),
-('Saman','Perera','saman@somewhere.lk', 'Szögöd','2020-01-01'),
-('Julian','Soman','j.soman@uniersity.edu.org','Szöged','2019-11-01'),
-('Telishia','Mathewsa','tm1@rose.lk','Szeged','2018-01-01')
+('DA123','Dinesh Asanka','Dineshasanka@gmail.com','Szegöd','2020-01-01'),
+('SP123','Saman Perera','saman@somewhere.lk', 'Szögöd','2020-01-01'),
+('JS123','Julian Soman','j.soman@uniersity.edu.org','Szöged','2019-11-01'),
+('TM123','Telishia Mathewsa','tm1@rose.lk','Szeged','2018-01-01')
 
 
 
